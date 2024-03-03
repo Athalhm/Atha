612cff6a52768f95961bff575a7f033901a21564
atha <athallah.az@gmail.com>
2024-03-03 23:03:43 +0700

    penambahan gui

---
+++ tp_mod3/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace tp_mod3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.buttonKirim = new System.Windows.Forms.Button();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+          
+            this.textBoxNama.BackColor = System.Drawing.Color.LightPink;
+            this.textBoxNama.Location = new System.Drawing.Point(36, 55);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(217, 20);
+            this.textBoxNama.TabIndex = 0;
+          
+            this.buttonKirim.BackColor = System.Drawing.Color.LightPink;
+            this.buttonKirim.Location = new System.Drawing.Point(269, 55);
+            this.buttonKirim.Name = "buttonKirim";
+            this.buttonKirim.Size = new System.Drawing.Size(91, 20);
+            this.buttonKirim.TabIndex = 1;
+            this.buttonKirim.Text = "Kirim";
+            this.buttonKirim.UseVisualStyleBackColor = false;
+            this.buttonKirim.Click += new System.EventHandler(this.buttonKirim_Click);
+           
+            this.labelHasil.BackColor = System.Drawing.Color.LightPink;
+            this.labelHasil.Location = new System.Drawing.Point(36, 93);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(324, 20);
+            this.labelHasil.TabIndex = 2;
+         
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 173);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.buttonKirim);
+            this.Controls.Add(this.textBoxNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.Button buttonKirim;
+        private System.Windows.Forms.Label labelHasil;
+    }
+}
---
+++ tp_mod3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace tp_mod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKirim_Click(object sender, EventArgs e)
+        {
+            string inputNama = textBoxNama.Text;
+
+            if (!string.IsNullOrWhiteSpace(inputNama))
+            {
+                labelHasil.Text = "Halo " + inputNama + " yang tampan dan berani";
+            }
+            else
+            {
+                MessageBox.Show("Mohon isi nama praktikan terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
